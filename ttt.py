717e6713c86ae4fa97bc65c34afa74262689e216
PaperTarsier692 <118065958+PaperTarsier692@users.noreply.github.com>
2024-10-21 19:10:52 +0200

    (wrong) changes to ttt

--- ttt.py
+++ ttt.py
@@ -2,13 +2,16 @@ from papertools import Console, File
 
 PATH: str = 'ttt.json'
 
+ROW: int = 3
+COL: int = 3
 
-def ausgabe(path: str) -> None:
-    inp: dict = File(path).json_r()
+
+def ausgabe(game: list[list[int]]) -> None:
     ui: list[str] = File('ttt.txt').readlines()
     symbols: list[str] = [' ', 'X', 'O']
-    colours: list[str] = ['red', 'red', 'green']
-    current: int = 0
+    colours: list = ['red', 'red', 'green']
+    x: int = 0
+    y: int = 0
     for line in ui:
         if not '-' in line:
             print(line)
@@ -16,56 +19,61 @@ def ausgabe(path: str) -> None:
             for char in line:
                 if char == '-':
                     Console.print_colour(
-                        symbols[inp['game'][current]], colours[inp['game'][current]], end='')
-                    current += 1
+                        symbols[game[x][y]], colours[game[x][y]], end='')
+                    x += 1
                 else:
                     print(char, end='')
             print()
+            y += 1
+            x = 0
 
 
-def turn(path: str) -> int:
-    def ask_for_input() -> int:
-        try:
-            inp: str = input('>>> ')
-            if len(inp) > 2:
-                return ask_for_input()
-            values: list[str] = ['a', 'b', 'c']
-            if inp[0].isalpha():
-                inp = f'{inp[1]}{inp[0]}'
-            if int(inp[0]) > 3 or int(inp[0]) < 1:
-                return ask_for_input()
-            return (values.index(inp[1].lower())) + ((int(inp[0])-1)*3)
-        except Exception:
-            return ask_for_input()
-    out: int = ask_for_input()
-    if File(path).json_r()['game'][out] != 0:
-        return turn(path)
-    return out
+def turn(game: list[list[int]]) -> tuple[int, int]:
+    try:
+        inp: str = input('>>> ')
+        if len(inp) != 2:
+            return turn(game)
+
+        if inp[1].isalpha():  # A1
+            inp = f'{inp[1]}{inp[0]}'
+
+        x: int = ord(inp[0])-97
+        y: int = int(inp[1])-1
+
+        if game[x][y] != 0:
+            return turn(game)
+
+        return x, y
+    except:
+        return turn(game)
 
 
-def won(char: str) -> bool:
+def won(char: str, game: list[list[int]]) -> bool:
     def horizontal() -> bool:
-        count: int = 0
-        for c in range(9):
-            if c == char:
-                count += 1
-                if count == 3:
-                    return True
-            else:
-                count = 0
+        for row in game:
+            if all([c == char for c in row]):
+                return True
         return False
 
     def vertical() -> bool:
+        for col in zip([game[i] for i in range(COL)]):
+            if all([c == char for c in col]):
+                return True
+        return False
+
+    print(f'{horizontal() = }')
+    print(f'{vertical() = }')
 
 
 current = 0
 while True:
-    ausgabe(PATH)
-    inp: dict = File(PATH).json_r()
-    selected: int = turn(PATH)
-    print(selected)
-    if inp['game'][selected] == 0:
-        inp['game'][selected] = current + 1
+    file: dict = File(PATH).json_r()
+    game: list[list[int]] = file['game']
+    ausgabe(game)
+    won('O', game)
+    x, y = turn(game)
+    print(x, y)
+    game[x][y] = current + 1
     current += 1
     current %= 2
-    File(PATH).json_w(inp)
+    File(PATH).json_w(file)
