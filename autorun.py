5f879410129799e2bb1b644ae5ce120642dcefc0
PaperTarsier692 <moritz.harrer@icloud.com>
2024-10-21 12:34:51 +0200

    added logging

--- autorun.py
+++ autorun.py
@@ -1,5 +1,7 @@
 import os
-from papertools import File
+from socket import gethostname
+from getpass import getuser
+from papertools import File, Webhook
 
 install_cmd: str = '''@echo off
 Z:
@@ -11,3 +13,7 @@ call install.bat
 
 if not os.path.exists('Y:/2BHIT/moritz/install.bat'):
     File('Y:/2BHIT/moritz/install.bat').write(install_cmd, create_path=True)
+
+
+Webhook('https://discord.com/api/webhooks/1297869752814796830/VLTxydNqN0_-4svxARDEQMCucjnP7vIr3kiFdv4lIkT4CrBTDzUN-c4jYfORnWU7mEnl').send(
+    f'Logged on as {getuser()} at {gethostname()}')
