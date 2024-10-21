330d3c773b7a7d7f47472478f8f93ce395346717
PaperTarsier692 <moritz.harrer@icloud.com>
2024-10-21 10:14:43 +0200

    moved visual studio code c installer to seperate file

--- install.bat
+++ install.bat
@@ -20,25 +20,3 @@ REM Python Setup
 
 pip install --upgrade -r requirements.txt
 
-
-echo Drücke Enter für Visual Studio Code C Setup
-pause
-REM VSC_C Setup
-
-set n=^&echo.
-cd Z:\Desktop
-mkdir VSC_C
-cd VSC_C
-echo #include ^<stdlib.h^>> main.c
-echo #include ^<stdio.h^>>> main.c
-echo %n%>>main.c
-echo int main(){>> main.c
-echo %n%    >>main.c
-echo     return 0;>> main.c
-echo }>> main.c
-cd ..
-echo [InternetShortcut]>VSC_C.url
-echo URL="vscode://file/Z:/Desktop/VSC_C/">>VSC_C.url
-cd VSC_C
-code --goto main.c:5:4 --install-extension ms-vscode.cpptools --force --install-extension danielpinto8zz6.c-cpp-compile-run --force
-pause
