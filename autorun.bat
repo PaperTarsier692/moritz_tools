66fe482a09c2d981c53fead335c71f306f67fe48
PaperTarsier692 <moritz.harrer@icloud.com>
2024-10-21 10:00:12 +0200

    pip install now quiet on autorun

--- autorun.bat
+++ autorun.bat
@@ -7,7 +7,7 @@ git pull | findstr /C:"Updating"
 if %errorlevel% equ 0 goto update
 
 setx Path "C:\Program Files\CodeBlocks\MinGW\bin; %USERPROFILE%\AppData\Local\Microsoft\WindowsApps"
-pip install -r requirements.txt
+pip install -r requirements.txt --quiet
 python autorun.py
 
 goto finish
