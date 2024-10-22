11b4b8560e82d8d5171a7f742d78a18f5cdbc7ef
PaperTarsier692 <moritz.harrer@icloud.com>
2024-10-22 21:43:24 +0200

    fixed ttt.py

--- ttt.py
+++ ttt.py
@@ -4,6 +4,7 @@ PATH: str = 'ttt.json'
 
 ROW: int = 3
 COL: int = 3
+NEEDED: int = 3
 
 
 def ausgabe(game: list[list[int]]) -> None:
@@ -48,31 +49,83 @@ def turn(game: list[list[int]]) -> tuple[int, int]:
         return turn(game)
 
 
-def won(char: str, game: list[list[int]]) -> bool:
-    def horizontal() -> bool:
-        for row in game:
-            if all([c == char for c in row]):
-                return True
-        return False
+def won(char_inp: str, game: list[list[int]]) -> bool:
+    char: int = [' ', 'X', 'O'].index(char_inp)
 
     def vertical() -> bool:
-        for col in zip([game[i] for i in range(COL)]):
-            if all([c == char for c in col]):
-                return True
+        for row in game:
+            count: int = 0
+            for c in row:
+                if c == char:
+                    count += 1
+                    if count == NEEDED:
+                        return True
+                else:
+                    count = 0
         return False
 
-    print(f'{horizontal() = }')
-    print(f'{vertical() = }')
+    def horizontal() -> bool:
+        ngame: list[list[int]] = [list(row) for row in zip(*game)]
+        for col in ngame:
+            count: int = 0
+            for c in col:
+                if c == char:
+                    count += 1
+                    if count == NEEDED:
+                        return True
+                else:
+                    count = 0
+        return False
 
+    def diagonal() -> bool:
+        for i in range(ROW):
+            for j in range(COL):
+                if i+NEEDED <= ROW and j+NEEDED <= COL:
+                    count: int = 0
+                    for k in range(NEEDED):
+                        if game[i+k][j+k] == char:
+                            count += 1
+                            if count == NEEDED:
+                                return True
+                        else:
+                            count = 0
+                if i+NEEDED <= ROW and j-NEEDED >= -1:
+                    count: int = 0
+                    for k in range(NEEDED):
+                        if game[i+k][j-k] == char:
+                            count += 1
+                            if count == NEEDED:
+                                return True
+                        else:
+                            count = 0
+        return False
+
+    return vertical() or horizontal() or diagonal()
+
+
+def new_game() -> dict:
+    return {'p1': input('<<<Spieler 1: >>> '), 'p2': input('<<<Spieler 2: >>> '), 'game': [[0 for _ in range(COL)] for _ in range(ROW)], 'current': 1}
+
+
+File(PATH).json_w(new_game())
 
 current = 0
+
+file: dict = File(PATH).json_r()
+
+p1: str = file['p1']
+p2: str = file['p2']
+
+
 while True:
-    file: dict = File(PATH).json_r()
+    file = File(PATH).json_r()
     game: list[list[int]] = file['game']
     ausgabe(game)
-    won('O', game)
+    if won('X', game):
+        print(f'{p1} hat gewonnen!!!')
+    elif won('O', game):
+        print(f'{p2} hat gewonnen!!!')
     x, y = turn(game)
-    print(x, y)
     game[x][y] = current + 1
     current += 1
     current %= 2
