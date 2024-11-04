be59dd48dc1428cebdc6cae83b0cd7d2b2e09f25
PaperTarsier692 <moritz.harrer@icloud.com>
2024-11-04 10:02:21 +0100

    removed --silent when pip installing

--- autorun.bat
+++ autorun.bat
@@ -8,7 +8,7 @@ git pull | findstr /C:"Updating"
 if %errorlevel% equ 0 goto update
 
 setx Path "C:\Program Files\CodeBlocks\MinGW\bin; %USERPROFILE%\AppData\Local\Microsoft\WindowsApps"
-pip install -r requirements.txt --quiet
+pip install -r requirements.txt
 python autorun.py
 
 goto finish
