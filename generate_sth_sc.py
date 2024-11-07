3def403ffac74bda2d5e8dbb5c75d27a4e83c542
PaperTarsier692 <moritz.harrer@icloud.com>
2024-11-07 06:54:38 +0100

    added ability to send folders with send_to_home_sc

--- generate_sth_sc.py
+++ generate_sth_sc.py
@@ -6,3 +6,7 @@ from sys import executable
 fc = menus.FastCommand('Send To Home', type='FILES',
                        command=f'Z: && cd Z:\\Documents\\moritz_tools && "{executable}" "Z:\\Documents\\moritz_tools\\send_to_home_sc.py" ?', command_vars=['FILENAME'])
 fc.compile()
+
+fc2 = menus.FastCommand('Send To Home', type='DIRECTORY',
+                        command=f'Z: && cd Z:\\Documents\\moritz_tools && "{executable}" "Z:\\Documents\\moritz_tools\\send_to_home_sc.py" ?', command_vars=['DIRECTORY'])
+fc2.compile()
--- send_to_home_sc.py
+++ send_to_home_sc.py
@@ -1,7 +1,8 @@
-from mt import ensure_venv
+from mt import ensure_venv, y_n
 from sys import argv
 ensure_venv(__file__, argv)
 
+import os
 from papertools import File, Webhook
 
 path: str = argv[1]
@@ -13,7 +14,21 @@ if not File('settings.json').exists():
     exit()
 
 settings: dict = File('settings.json').json_r()
-
 wh: Webhook = Webhook(settings['url'], settings['username'])
+
+if os.path.isdir(path):
+    zip_path: str = f"{path}.zip"
+    if File(zip_path).exists():
+        if y_n(f'Es gibt bereits eine Datei {zip_path}, soll sie überschrieben werden?'):
+            os.remove(zip_path)
+        else:
+            exit()
+    from shutil import make_archive
+    print("Ordner erkannt, komprimiert Ordner...")
+    make_archive(path,
+                 'zip', path)
+    wh.send_file(zip_path)
+    os.remove(zip_path)
+
 
 wh.send_file(path)
