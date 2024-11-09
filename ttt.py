5fbd2183aae48552646cabffbce005a27befed5e
PaperTarsier692 <moritz.harrer@icloud.com>
2024-11-09 21:19:38 +0100

    made ttt output work with extended rows/columns

--- ttt.py
+++ ttt.py
@@ -11,25 +11,33 @@ NEEDED: int = 3
 
 
 def ausgabe(game: list[list[int]]) -> None:
-    ui: list[str] = File('ttt.txt').readlines()
     symbols: list[str] = [' ', 'X', 'O']
     colours: list = ['red', 'red', 'green']
     x: int = 0
     y: int = 0
-    for line in ui:
-        if '-' not in line:
-            print(line)
+    for y in range(COL):
+        print(f'   {chr(y + 97)}  ', end='')
+    print()
+    for x in range(ROW):
+        print(' ', end='')
+        for y in range(COL - 1):
+            print('     |', end='')
+        print(f'\n{x + 1}  ', end='')
+        Console.print_colour(
+            f'{symbols[game[0][x]]}  ', colours[game[0][x]], end='')
+        for y in range(COL - 1):
+            print(f'|  ', end='')
+            Console.print_colour(
+                f'{symbols[game[y + 1][x]]}  ', colours[game[y + 1][x]], end='')
+        print('\n ', end='')
+
+        if x != ROW - 1:
+            for y in range(COL):
+                print('_____|' if y != COL - 1 else '_____', end='')
         else:
-            for char in line:
-                if char == '-':
-                    Console.print_colour(
-                        symbols[game[x][y]], colours[game[x][y]], end='')
-                    x += 1
-                else:
-                    print(char, end='')
-            print()
-            y += 1
-            x = 0
+            for y in range(COL - 1):
+                print('     |', end='')
+        print()
 
 
 def turn(game: list[list[int]]) -> tuple[int, int]:
@@ -107,6 +115,9 @@ def won(char_inp: str, game: list[list[int]]) -> bool:
 
 
 def new_game() -> dict:
+    return {'p1': 'SP1', 'p2': "SP2",
+            'game': [[0 for _ in range(COL)]
+                     for _ in range(ROW)], 'current': 1}
     return {'p1': input('<<<Spieler 1: >>> '),
             'p2': input('<<<Spieler 2: >>> '),
             'game': [[0 for _ in range(COL)]
