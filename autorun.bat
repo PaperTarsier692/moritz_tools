0f0feba1f33512176198f174254545ae0de9e0ca
PaperTarsier692 <moritz.harrer@icloud.com>
2024-11-11 09:26:30 +0100

    create test dir on start

--- autorun.bat
+++ autorun.bat
@@ -7,6 +7,8 @@ if %errorlevel% equ 0 goto update
 
 setx Path "C:\Program Files\CodeBlocks\MinGW\bin; %USERPROFILE%\AppData\Local\Microsoft\WindowsApps"
 
+mkdir Y:\2BHIT\test
+
 call .venv\Scripts\activate.bat
 pip install -r requirements.txt
 python autorun.py
