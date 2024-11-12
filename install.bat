00b86b1af6c3da0eb4a5dd0c4589d89e30e85be2
PaperTarsier692 <moritz.harrer@icloud.com>
2024-11-12 07:05:41 +0100

    removed install.bat comments

--- install.bat
+++ install.bat
@@ -1,21 +1,15 @@
 Z:
 
-REM Startup Skripte
-
 echo Z: > "Z:\Start Menu\Programs\Startup\moritz.bat"
 echo cd Z:\Documents\moritz_tools >> "Z:\Start Menu\Programs\Startup\moritz.bat"
 echo autorun.bat >> "Z:\Start Menu\Programs\Startup\moritz.bat"
 
-REM Setup
 cd Z:\Documents\moritz_tools
 git pull
-
-REM Python Setup
 
 python -m venv .venv
 call .venv\Scripts\activate.bat
 pip install --upgrade -r requirements.txt
-
 
 call autorun.bat
 del "Z:\Start Menu\Programs\Startup\vsc_c.bat"
