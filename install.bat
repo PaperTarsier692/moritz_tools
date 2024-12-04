b3b75b620442e65e2ca569ca4da46f6b428d1579
PaperTarsier692 <moritz.harrer@icloud.com>
2024-12-04 10:38:13 +0100

    moved new autorun shortcut

--- install.bat
+++ install.bat
@@ -1,13 +1,14 @@
 Z:
 
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('Z:\Start Menu\Programs\Startup\moritz_tools.lnk');$s.TargetPath='Z:\Documents\moritz_tools\.venv\Scripts\pythonw.exe';$s.Arguments='Z:\Documents\moritz_tools\autorun.py';$s.IconLocation='Z:\Documents\moritz_tools\.venv\Scripts\pythonw.exe';$s.WorkingDirectory='Z:\Documents\moritz_tools';$s.WindowStyle=7;$s.Save()"
-
 cd Z:\Documents\moritz_tools
 git pull
 
 python -m venv .venv
 call .venv\Scripts\activate.bat
 pip install --upgrade -r requirements.txt
+
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('Z:\Start Menu\Programs\Startup\moritz_tools.lnk');$s.TargetPath='Z:\Documents\moritz_tools\.venv\Scripts\pythonw.exe';$s.Arguments='Z:\Documents\moritz_tools\autorun.py';$s.IconLocation='Z:\Documents\moritz_tools\.venv\Scripts\pythonw.exe';$s.WorkingDirectory='Z:\Documents\moritz_tools';$s.WindowStyle=7;$s.Save()"
+
 
 call autorun.bat
 del "Z:\Start Menu\Programs\Startup\vsc_c.bat"
