8a944fb78983a884fa7a379b1e425e5e60734b40
PaperTarsier692 <moritz.harrer@icloud.com>
2025-01-10 09:48:58 +0100

    added more prints, dont exit the program on crash

--- vsc_installer.py
+++ vsc_installer.py
@@ -6,29 +6,36 @@ import requests
 from papertools import Console
 from getpass import getuser
 
-while True:
-    processes: list[str] = os.popen(
-        'wmic process get description').read().splitlines()
-    processes = [process.strip() for process in processes if process != '']
+try:
+    while True:
+        processes: list[str] = os.popen(
+            'wmic process get description').read().splitlines()
+        processes = [process.strip() for process in processes if process != '']
 
-    if 'Code.exe' in processes:
-        Console.print_colour(
-            'Visual Studio Code rennt noch, bitte schließe es um Probleme zu vermeiden.', 'red')
-        inp: str = input()
-        if inp == 'c':
+        if 'Code.exe' in processes:
+            Console.print_colour(
+                'Visual Studio Code rennt noch, bitte schließe es um Probleme zu vermeiden.', 'red')
+            inp: str = input()
+            if inp == 'c':
+                break
+        else:
             break
+
+    Console.print_colour(
+        'Lädt VSC herunter...', 'red')
+
+    response: requests.Response = requests.get(
+        'https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user')
+
+    if response.status_code == 200:
+        with open('Z:\\Downloads\\vsc.exe', 'wb') as f:
+            f.write(response.content)
+        Console.print_colour('VSC heruntergeladen', 'green')
     else:
-        break
+        Console.print_colour('VSC konnte nicht heruntergeladen werden', 'red')
 
-response: requests.Response = requests.get(
-    'https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user')
+    os.system(
+        f'Z:\\Downloads\\vsc.exe /SP- /LOG="log.txt" /NOCANCEL /NORESTART /FORCECLOSEAPPLICATIONS /DIR="C:\\Users\\{getuser()}\\vsc\\')
 
-if response.status_code == 200:
-    with open('Z:\\Downloads\\vsc.exe', 'wb') as f:
-        f.write(response.content)
-    print('VSC heruntergeladen')
-else:
-    print('VSC konnte nicht heruntergeladen')
-
-os.system(
-    f'Z:\\Downloads\\vsc.exe /SP- /LOG="log.txt" /NOCANCEL /NORESTART /FORCECLOSEAPPLICATIONS /DIR="C:\\Users\\{getuser()}\\vsc\\')
+finally:
+    input()
