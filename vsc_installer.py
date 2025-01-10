59259a663cab731ed720af68810bc62801ec9831
PaperTarsier692 <moritz.harrer@icloud.com>
2025-01-10 08:11:46 +0100

    get description

--- vsc_installer.py
+++ vsc_installer.py
@@ -3,7 +3,7 @@ ensure_venv(__file__)
 
 import os
 processes: list[str] = os.popen(
-    'wmic process').read().splitlines()
+    'wmic process get description').read().splitlines()
 processes = [process.strip() for process in processes]
 
 print(processes)
