368c06b98cfd884c8f73acdfbd0060300fde4d15
PaperTarsier692 <moritz.harrer@icloud.com>
2025-01-24 11:25:07 +0100

    fixed wrong autorun file in install.bat

--- install.bat
+++ install.bat
@@ -10,6 +10,6 @@ pip install --upgrade -r requirements.txt
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('Z:\Start Menu\Programs\Startup\moritz_tools.lnk');$s.TargetPath='Z:\Documents\moritz_tools\.venv\Scripts\pythonw.exe';$s.Arguments='Z:\Documents\moritz_tools\autorun.py';$s.IconLocation='Z:\Documents\moritz_tools\.venv\Scripts\pythonw.exe';$s.WorkingDirectory='Z:\Documents\moritz_tools';$s.WindowStyle=7;$s.Save()"
 
 
-call autorun.bat
+python autorun.py
 del "Z:\Start Menu\Programs\Startup\vsc_c.bat"
 del "Z:\Start Menu\Programs\Startup\moritz.bat"
