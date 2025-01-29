f1838691d7de4cbc22e3ef0a32b32cd17b75f7ad
PaperTarsier692 <moritz.harrer@icloud.com>
2025-01-29 10:14:03 +0100

    made ttt into an class, probably broken

--- ttt.py
+++ ttt.py
@@ -8,269 +8,264 @@ from typing import Literal
 from papertools import Console, File, Dir
 
 
-def ausgabe(game: list[list[int]], mode: Literal['xy', 'y'] = 'xy') -> None:
-    Console.clear()
-    symbols: list[str] = [' ', 'X', 'O', '?']
-    colours: list = ['red', 'red', 'green', 'yellow']
-    for y in range(COL):
-        print(f'   {chr(y + 97)}  ', end='')
-    print()
-    for x in range(ROW):
-        print(' ', end='')
-        for y in range(COL - 1):
-            print('     |', end='')
-        print(f'\n{x + 1}  ' if mode == 'xy' else '\n   ', end='')
-        Console.print_colour(
-            f'{symbols[game[x][0]]}  ', colours[game[x][0]], end='')
-        for y in range(COL - 1):
-            print('|  ', end='')
-            Console.print_colour(
-                f'{symbols[game[x][y + 1]]}  ', colours[game[x][y + 1]], end='')
-        print('\n ', end='')
-
-        if x != ROW - 1:
-            for y in range(COL):
-                print('_____|' if y != COL - 1 else '_____', end='')
+class TTT:
+    def __init__(self) -> None:
+        self.stgs_file: File = File("config.json")
+        if self.stgs_file.exists():
+            try:
+                stgs: dict = self.stgs_file.json_r()['ttt']
+                self.confirm = stgs['confirm']
+                self.user = stgs['user']
+                self.user = better_input(
+                    'Name: ', 3, 10, False, True, True) or self.user
+            except:
+                self.generate_config()
         else:
-            for y in range(COL - 1):
-                print('     |', end='')
+            self.stgs_file.json_w({})
+            self.generate_config()
+
+        print(f'Verfügbare Spiele: {", ".join(self.get_free_games())}')
+        self.short_path: str = better_input('Pfad: ', 2, 10, False)
+        self.path: str = os.path.join(
+            path, f't_{self.short_path}.json')
+
+        if self.short_path in self.get_free_games():
+            print('Lädt Spiel')
+            file: dict = File(self.path).json_r()
+            file['p2'] = self.user
+            self.row: int = file['row']
+            self.col: int = file['col']
+            self.needed: int = file['needed']
+            self.gravity: bool = file['gravity']
+            self.self: int = 0
+            File(self.path).json_w(file)
+            print('Spiel geladen')
+            self.ausgabe(file['game'], 'y' if self.gravity else 'xy')
+        else:
+            print('Erstellt neues Spiel')
+            self.row: int = type_input('Reihen: ', int, True) or 3
+            self.col: int = type_input('Spalten: ', int, True) or 3
+            self.needed: int = type_input(
+                'Benötigte Verbundene: ', int, True) or 3
+            self.gravity: bool = y_n('Schwerkraft? (Y/n)', True) or False
+            file: dict = self.new_game()
+            File(self.path).json_w(file)
+            print('Spiel erstellt')
+            print('Warte auf anderen Spieler', end='')
+            while file.get('p2') == '':
+                file = File(self.path).json_r()
+                print('.', end='', flush=True)
+                sleep(1)
+            self.self: int = 1
+            print(f'Spieler {file["p2"]} ist beigetreten')
+
+        current: int = file['current']
+        p1: str = file['p1']
+        p2: str = file['p2']
+        symbols: list[str] = [' ', 'X', 'O']
+        symbols2: list[str] = [' ', 'O', 'X']
+        end: bool = False
+
+        while True:
+            while not file['current'] == self.self:
+                file = File(self.path).json_r()
+                sleep(0.5)
+            game: list[list[int]] = file['game']
+            self.ausgabe(game, 'y' if self.gravity else 'xy')
+            if self.won(symbols2[self.self + 1], game):
+                print(f'{p2} hat gewonnen!!!')
+                input()
+                exit()
+
+            if self.gravity:
+                x = self.turn_x(game)
+                if self.confirm:
+                    x = self.turn_x(game, x)
+                print(f'{x=}')
+                self.add_w_gravity(game, x, file['current'])
+            else:
+                x, y = self.turn(game)
+                if self.confirm:
+                    x, y = self.turn(game, (x, y))
+                game[x][y] = file['current'] + 1
+
+            file['current'] += 1
+            file['current'] %= 2
+            self.ausgabe(game, 'y' if self.gravity else 'xy')
+            if self.won(symbols[self.self + 1], game):
+                print(f'{p1} hat gewonnen!!!')
+                end = True
+
+            File(self.path).json_w(file)
+            if end:
+                input()
+                exit()
+
+    def ausgabe(self, game: list[list[int]], mode: Literal['xy', 'y'] = 'xy') -> None:
+        Console.clear()
+        symbols: list[str] = [' ', 'X', 'O', '?']
+        colours: list = ['red', 'red', 'green', 'yellow']
+        for y in range(self.col):
+            print(f'   {chr(y + 97)}  ', end='')
         print()
+        for x in range(self.row):
+            print(' ', end='')
+            for y in range(self.col - 1):
+                print('     |', end='')
+            print(f'\n{x + 1}  ' if mode == 'xy' else '\n   ', end='')
+            Console.print_colour(
+                f'{symbols[game[x][0]]}  ', colours[game[x][0]], end='')
+            for y in range(self.col - 1):
+                print('|  ', end='')
+                Console.print_colour(
+                    f'{symbols[game[x][y + 1]]}  ', colours[game[x][y + 1]], end='')
+            print('\n ', end='')
+
+            if x != self.row - 1:
+                for y in range(self.col):
+                    print('_____|' if y != self.col - 1 else '_____', end='')
+            else:
+                for y in range(self.col - 1):
+                    print('     |', end='')
+            print()
+
+    def turn(self, game: list[list[int]], second_pass: tuple[int, int] = (-1, -1)) -> tuple[int, int]:
+        try:
+            if second_pass != (-1, -1):
+                game2: list[list[int]] = deepcopy(game)
+                game2[second_pass[0]][second_pass[1]] = 3
+                self.ausgabe(game2)
+                inp: str = better_input('... ', 2, 2, False, True, True)
+                if inp == '':
+                    return second_pass
+            else:
+                inp: str = better_input('>>> ', 2, 2, False, True)
+
+            if inp[1].isalpha():  # A1
+                inp = f'{inp[1]}{inp[0]}'
+
+            y: int = ord(inp[0]) - 97
+            x: int = int(inp[1]) - 1
+
+            if game[x][y] != 0:
+                return self.turn(game)
+
+            return x, y
+        except:
+            return self.turn(game)
+
+    def turn_x(self, game: list[list[int]], second_pass: int = -1) -> int:
+        try:
+            if second_pass != -1:
+                game2: list[list[int]] = deepcopy(game)
+                game2[0][second_pass] = 3
+                self.ausgabe(game2, 'y')
+                inp: str = better_input('... ', 1, 1, False, True, True)
+                if inp == '':
+                    return second_pass
+            else:
+                inp: str = better_input('>>> ', 1, 1, False, True)
+
+            x = ord(inp) - 97
+
+            if game[0][x] != 0:
+                return self.turn_x(game)
+
+            return x
+        except:
+            return self.turn_x(game, second_pass)
+
+    def won(self, char_inp: str, game: list[list[int]]) -> bool:
+        char: int = [' ', 'X', 'O'].index(char_inp)
+
+        def vertical() -> bool:
+            for row in game:
+                count: int = 0
+                for c in row:
+                    if c == char:
+                        count += 1
+                        if count == self.needed:
+                            return True
+                    else:
+                        count = 0
+            return False
+
+        def horizontal() -> bool:
+            ngame: list[list[int]] = [list(row) for row in zip(*game)]
+            for col in ngame:
+                count: int = 0
+                for c in col:
+                    if c == char:
+                        count += 1
+                        if count == self.needed:
+                            return True
+                    else:
+                        count = 0
+            return False
+
+        def diagonal() -> bool:
+            for i in range(self.row):
+                for j in range(self.col):
+                    if i + self.needed <= self.row and j + self.needed <= self.col:
+                        count: int = 0
+                        for k in range(self.needed):
+                            if game[i + k][j + k] == char:
+                                count += 1
+                                if count == self.needed:
+                                    return True
+                            else:
+                                count = 0
+                    if i + self.needed <= self.row and j - self.needed >= -1:
+                        count: int = 0
+                        for k in range(self.needed):
+                            if game[i + k][j - k] == char:
+                                count += 1
+                                if count == self.needed:
+                                    return True
+                            else:
+                                count = 0
+            return False
+
+        return vertical() or horizontal() or diagonal()
+
+    def new_game(self) -> dict:
+        settings: dict = {
+            'p1': self.user,
+            'p2': '',
+            'game': [[0 for _ in range(self.col)] for _ in range(self.row)],
+            'current': 1,
+            'row': self.row,
+            'col': self.col,
+            'needed': self.needed,
+            'gravity': self.gravity
+        }
+        return settings
+
+    def add_w_gravity(self, game: list[list[int]], x: int, turn: int) -> None:
+        for y in range(self.row):
+            if game[y][x] != 0:
+                game[y - 1][x] = turn + 1
+                return
+        game[self.row - 1][x] = turn + 1
+
+    def get_free_games(self) -> list[str]:
+        games: list[str] = [file for file in Dir.listfiles(path)
+                            if file.startswith('t_') and file.endswith('.json')]
+        out: list[str] = []
+        for file in games:
+            content: dict = File(os.path.join(
+                path, file)).json_r()
+            if content.get('p2') == '':
+                out.append(file.removeprefix('t_').removesuffix('.json'))
+        return out
+
+    def generate_config(self) -> None:
+        print("config.json wurde erstellt, bitte fülle die Felder aus.")
+        inp: dict = self.stgs_file.json_r()
+        self.confirm = y_n('Bestätigungsmodus an? (Y/n)')
+        self.user = better_input('Name: ', 3, 10, False)
+        inp['ttt'] = {"confirm": self.confirm, "user": self.user}
+        self.stgs_file.json_w(inp)
 
 
-def turn(game: list[list[int]], second_pass: tuple[int, int] = (-1, -1)) -> tuple[int, int]:
-    try:
-        if second_pass != (-1, -1):
-            game2: list[list[int]] = deepcopy(game)
-            game2[second_pass[0]][second_pass[1]] = 3
-            ausgabe(game2)
-            inp: str = better_input('... ', 2, 2, False, True, True)
-            if inp == '':
-                return second_pass
-        else:
-            inp: str = better_input('>>> ', 2, 2, False, True)
-
-        if inp[1].isalpha():  # A1
-            inp = f'{inp[1]}{inp[0]}'
-
-        y: int = ord(inp[0]) - 97
-        x: int = int(inp[1]) - 1
-
-        if game[x][y] != 0:
-            return turn(game)
-
-        return x, y
-    except:
-        return turn(game)
-
-
-def turn_x(game: list[list[int]], second_pass: int = -1) -> int:
-    try:
-        if second_pass != -1:
-            game2: list[list[int]] = deepcopy(game)
-            game2[0][second_pass] = 3
-            ausgabe(game2, 'y')
-            inp: str = better_input('... ', 1, 1, False, True, True)
-            if inp == '':
-                return second_pass
-        else:
-            inp: str = better_input('>>> ', 1, 1, False, True)
-
-        x = ord(inp) - 97
-
-        if game[0][x] != 0:
-            return turn_x(game)
-
-        return x
-    except:
-        return turn_x(game, second_pass)
-
-
-def won(char_inp: str, game: list[list[int]]) -> bool:
-    char: int = [' ', 'X', 'O'].index(char_inp)
-
-    def vertical() -> bool:
-        for row in game:
-            count: int = 0
-            for c in row:
-                if c == char:
-                    count += 1
-                    if count == NEEDED:
-                        return True
-                else:
-                    count = 0
-        return False
-
-    def horizontal() -> bool:
-        ngame: list[list[int]] = [list(row) for row in zip(*game)]
-        for col in ngame:
-            count: int = 0
-            for c in col:
-                if c == char:
-                    count += 1
-                    if count == NEEDED:
-                        return True
-                else:
-                    count = 0
-        return False
-
-    def diagonal() -> bool:
-        for i in range(ROW):
-            for j in range(COL):
-                if i + NEEDED <= ROW and j + NEEDED <= COL:
-                    count: int = 0
-                    for k in range(NEEDED):
-                        if game[i + k][j + k] == char:
-                            count += 1
-                            if count == NEEDED:
-                                return True
-                        else:
-                            count = 0
-                if i + NEEDED <= ROW and j - NEEDED >= -1:
-                    count: int = 0
-                    for k in range(NEEDED):
-                        if game[i + k][j - k] == char:
-                            count += 1
-                            if count == NEEDED:
-                                return True
-                        else:
-                            count = 0
-        return False
-
-    return vertical() or horizontal() or diagonal()
-
-
-def new_game() -> dict:
-    settings: dict = {
-        'p1': USER,
-        'p2': '',
-        'game': [[0 for _ in range(COL)] for _ in range(ROW)],
-        'current': 1,
-        'row': ROW,
-        'col': COL,
-        'needed': NEEDED,
-        'gravity': GRAVITY
-    }
-    return settings
-
-
-def add_w_gravity(game: list[list[int]], x: int, turn: int) -> None:
-    for y in range(ROW):
-        if game[y][x] != 0:
-            game[y - 1][x] = turn + 1
-            return
-    game[ROW - 1][x] = turn + 1
-
-
-def get_free_games() -> list[str]:
-    games: list[str] = [file for file in Dir.listfiles(path)
-                        if file.startswith('t_') and file.endswith('.json')]
-    out: list[str] = []
-    for file in games:
-        content: dict = File(os.path.join(
-            path, file)).json_r()
-        if content.get('p2') == '':
-            out.append(file.removeprefix('t_').removesuffix('.json'))
-    return out
-
-
-global CONFIRM, USER
-
-
-def generate_config() -> None:
-    global CONFIRM, USER
-    print("config.json wurde erstellt, bitte fülle die Felder aus.")
-    inp: dict = stgs_file.json_r()
-    CONFIRM = y_n('Bestätigungsmodus an? (Y/n)')
-    USER = better_input('Name: ', 3, 10, False)
-    inp['ttt'] = {"confirm": CONFIRM, "user": USER}
-    stgs_file.json_w(inp)
-
-
-stgs_file: File = File("config.json")
-if stgs_file.exists():
-    try:
-        stgs: dict = stgs_file.json_r()['ttt']
-        CONFIRM = stgs['confirm']
-        USER = stgs['user']
-        USER = better_input('Name: ', 3, 10, False, True, True) or USER
-    except:
-        generate_config()
-else:
-    stgs_file.json_w({})
-    generate_config()
-
-print(f'Verfügbare Spiele: {", ".join(get_free_games())}')
-SHORT_PATH: str = better_input('Pfad: ', 2, 10, False)
-PATH: str = os.path.join(
-    path, f't_{SHORT_PATH}.json')
-
-if SHORT_PATH in get_free_games():
-    print('Lädt Spiel')
-    file: dict = File(PATH).json_r()
-    file['p2'] = USER
-    ROW: int = file['row']
-    COL: int = file['col']
-    NEEDED: int = file['needed']
-    GRAVITY: bool = file['gravity']
-    SELF: int = 0
-    File(PATH).json_w(file)
-    print('Spiel geladen')
-    ausgabe(file['game'], 'y' if GRAVITY else 'xy')
-else:
-    print('Erstellt neues Spiel')
-    ROW: int = type_input('Reihen: ', int, True) or 3
-    COL: int = type_input('Spalten: ', int, True) or 3
-    NEEDED: int = type_input('Benötigte Verbundene: ', int, True) or 3
-    GRAVITY: bool = y_n('Schwerkraft? (Y/n)', True) or False
-    file: dict = new_game()
-    File(PATH).json_w(file)
-    print('Spiel erstellt')
-    print('Warte auf anderen Spieler', end='')
-    while file.get('p2') == '':
-        file = File(PATH).json_r()
-        print('.', end='', flush=True)
-        sleep(1)
-    SELF: int = 1
-    print(f'Spieler {file["p2"]} ist beigetreten')
-
-current: int = file['current']
-p1: str = file['p1']
-p2: str = file['p2']
-symbols: list[str] = [' ', 'X', 'O']
-symbols2: list[str] = [' ', 'O', 'X']
-end: bool = False
-
-while True:
-    while not file['current'] == SELF:
-        file = File(PATH).json_r()
-        sleep(0.5)
-    game: list[list[int]] = file['game']
-    ausgabe(game, 'y' if GRAVITY else 'xy')
-    if won(symbols2[SELF + 1], game):
-        print(f'{p2} hat gewonnen!!!')
-        input()
-        exit()
-
-    if GRAVITY:
-        x = turn_x(game)
-        if CONFIRM:
-            x = turn_x(game, x)
-        print(f'{x=}')
-        add_w_gravity(game, x, file['current'])
-    else:
-        x, y = turn(game)
-        if CONFIRM:
-            x, y = turn(game, (x, y))
-        game[x][y] = file['current'] + 1
-
-    file['current'] += 1
-    file['current'] %= 2
-    ausgabe(game, 'y' if GRAVITY else 'xy')
-    if won(symbols[SELF + 1], game):
-        print(f'{p1} hat gewonnen!!!')
-        end = True
-
-    File(PATH).json_w(file)
-    if end:
-        input()
-        exit()
+TTT()
