0e61d161e8e784fe143b0f1e5d5d4a67f0c86c7c
PaperTarsier692 <moritz.harrer@icloud.com>
2025-09-26 09:43:21 +0200

    removed old chat java files

---
+++ meh.py
@@ -0,0 +1,5 @@
+from papertools import Webhook
+
+wh = Webhook('https://discord.com/api/webhooks/1420659312300134440/LZWBbxRkTFEsisUO2WGjEb8GTQUN9uRucHP8cOdN3_y0uUSFIswqTTKBVU2WC04Ksdq5')
+
+wh.send('meh', thread_id=1394349588260851747)
